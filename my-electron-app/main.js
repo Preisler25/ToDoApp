51e17ea0d887093d4d08905901177a11533a4204
Preisler <preisler.andras@gmail.com>
2023-01-30 23:19:17 +0100

    test

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,24 +1,41 @@
-const { app, BrowserWindow } = require('electron')
-const {ipcMain} = require('electron')
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+let mainWindow;
 
-ipcMain.on('form-submission', (event, arg) => {
-  console.log(event);
-  console.log(arg) //ez fogja megjeleníteni a bevitt adatot
-})
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    fullscreen: app.isPackaged,
+    webPreferences: {
+      'web-security': false,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+  mainWindow.loadFile(path.join(__dirname, './frontend/index.html'));
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-    console.log("createWindow");
-    win.loadFile('index.html')
+  if (!app.isPackaged) {
+    // Running in debug
+    mainWindow.webContents.openDevTools();
+    console.log('Have fun Debugging!');
+  } else {
+    // Running in production
+    Menu.setApplicationMenu(false);
   }
+}
 
-  app.whenReady().then(() => {
-    createWindow()
-  })
 
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
